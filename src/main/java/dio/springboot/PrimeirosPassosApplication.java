d9d2969dac0e9a884fa236f351d7d70c2b4ca028
Artemiza Rocha <miza-pma@hotmail.com>
2022-07-07 11:42:40 -0300

    Criando uma aplicaçaõ

---
+++ src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -0,0 +1,17 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
+		Calculadora calculadora = new Calculadora();
+
+
+	}
+
+}
